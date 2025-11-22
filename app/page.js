f29a7b7c79b1b2fd09ec0e18dd4d135dcab91e52
Alomgir Hossen <alomgirsojol0005@gmail.com>
2025-11-23 04:39:49 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 "use client";
 import Scene from '../components/Scene';
-import '../styles/globals.css';
 
 export default function Page() {
-  return <Scene />;
+  return (
+    <div className="w-full h-screen">
+      <Scene />
+      <div className="pointer-events-none fixed bottom-6 left-1/2 -translate-x-1/2 text-white/40 text-xs">
+        Scroll to zoom • Drag to orbit • Pinch to zoom (mobile)
+      </div>
+    </div>
+  );
 }
